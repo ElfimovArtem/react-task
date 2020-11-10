d0b7f6aca32738db0659bbf5d19352585832d0f0
Artem_Elfimov <Artem_Elfimov@epam.com>
2020-11-10 19:23:51 +0300

    feat: Implemented a form for adding a movie

--- src/components/add-movie-form/add-movie-form.tsx
+++ src/components/add-movie-form/add-movie-form.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { Field, Form, Formik, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import { useDispatch } from 'react-redux';
 import { addYourMovie } from '../../constants';
 import { Button } from '../button';
+import { homeBtnClick } from '../../redux/adding-movie/actions';
 import './add-movie-form-styles.css';
 
+const defaultSchema = Yup.string()
+    .min(3, 'Too Short!')
+    .max(20, 'Too Long!')
+    .required('Required');
+
+const AddMovieSchema = Yup.object().shape({
+  Title: defaultSchema,
+  Type: defaultSchema,
+  Poster: defaultSchema,
+  Year: defaultSchema,
+  imdbID: defaultSchema
+});
+
 export const AddMovieForm = () => {
+  const put = useDispatch();
+
   return (
     <Formik
       initialValues = {
@@ -17,18 +34,16 @@ export const AddMovieForm = () => {
           imdbID: ''
         }
       }
-      validationSchema={
-        Yup.object({
-          // text: Yup.string()
-          //   .max(20, 'Must be 20 characters or less')
-          //   .min(3, 'Must be at least 3 characters long')
-          //   .required('This is a required field...')
-        })
-      }
-      onSubmit={
+      validationSchema = {AddMovieSchema}
+      onSubmit = {
         (values, { setFieldValue }) => {
           console.log(values.Title);
-          setFieldValue('Title', ''); //Очистит поле после отправки
+          setFieldValue('Title', '');
+          setFieldValue('Type', '');
+          setFieldValue('Poster', '');
+          setFieldValue('Year', '');
+          setFieldValue('imdbID', '');
+          put(homeBtnClick());
         }
       }
     >
@@ -39,40 +54,81 @@ export const AddMovieForm = () => {
           autoComplete="off"
           name='Title'
           type='text'
+          placeholder='Enter title'
         />
         <Field
           className='add-movie-form__input-Type'
           autoComplete="off"
           name='Type'
           type='text'
+          placeholder='Enter type'
         />
         <Field
           className='add-movie-form__input-Poster'
           autoComplete="off"
           name='Poster'
           type='text'
+          placeholder='Enter poster URL'
         />
         <Field
           className='add-movie-form__input-Year'
           autoComplete="off"
           name='Year'
           type='text'
+          placeholder='Enter films year'
         />
         <Field
           className='add-movie-form__input-imdbID'
           autoComplete="off"
           name='imdbID'
           type='text'
+          placeholder='Enter uniq ID'
         />
-        {/*<ErrorMessage name='text'>*/}
-        {/*  {*/}
-        {/*    msg => (*/}
-        {/*      <div className='error-validation'>*/}
-        {/*        {msg}*/}
-        {/*      </div>*/}
-        {/*    )*/}
-        {/*  }*/}
-        {/*</ErrorMessage>*/}
+        <ErrorMessage name='Title'>
+          {
+            msg => (
+              <div className='error-title-validation'>
+                {msg}
+              </div>
+            )
+          }
+        </ErrorMessage>
+        <ErrorMessage name='Type'>
+          {
+            msg => (
+              <div className='error-type-validation'>
+                {msg}
+              </div>
+            )
+          }
+        </ErrorMessage>
+        <ErrorMessage name='Poster'>
+          {
+            msg => (
+              <div className='error-poster-validation'>
+                {msg}
+              </div>
+            )
+          }
+        </ErrorMessage>
+        <ErrorMessage name='Year'>
+          {
+            msg => (
+              <div className='error-year-validation'>
+                {msg}
+              </div>
+            )
+          }
+        </ErrorMessage>
+        <ErrorMessage name='imdbID'>
+          {
+            msg => (
+              <div className='error-imdbID-validation'>
+                {msg}
+              </div>
+            )
+          }
+        </ErrorMessage>
         <div className='add-movie-form__submit'>
           <div className='add-movie-form__submit-container'>
             <Button
